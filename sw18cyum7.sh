c4700bdeaa9592265c37ca26426163bec8fd62b1
kulico <42776014+kulico@users.noreply.github.com>
2018-10-13 02:01:12 +0700

    deeja3

---
+++ sw18cyum7.sh
@@ -0,0 +1,3 @@
+unzip -oq /home/oracle/LINUX.X64_180000_db_home.zip /u01/app/oracle/product/18.0.0/dbhome_1/
+chown -R oracle:oinstall /u01/app/oracle/product/18.0.0/dbhome_1/
+chmod 775 runInstaller
