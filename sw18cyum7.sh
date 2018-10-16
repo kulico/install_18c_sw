253d30672ead287894c42963022bbc1e9d7f4a40
kulico <42776014+kulico@users.noreply.github.com>
2018-10-16 14:17:02 +0700

    Update sw18cyum7.sh

--- sw18cyum7.sh
+++ sw18cyum7.sh
@@ -1,3 +1,4 @@
-unzip -oq /home/oracle/LINUX.X64_180000_db_home.zip /u01/app/oracle/product/18.0.0/dbhome_1/
+cd /u01/app/oracle/product/18.0.0/dbhome_1/
+unzip -oq /home/oracle/LINUX.X64_180000_db_home.zip 
 chown -R oracle:oinstall /u01/app/oracle/product/18.0.0/dbhome_1/
 chmod 775 /u01/app/oracle/product/18.0.0/dbhome_1/runInstaller
